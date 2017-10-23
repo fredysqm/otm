331bb2cbbe3176b79711dd0e62b6804016d206c5
Fredy Quispe <fredysqm@gmail.com>
2017-10-23 09:28:16 -0500

    herramientas 3rd y debug

---
+++ otm/settings/common.py
@@ -0,0 +1,53 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
+ROOT_URLCONF = 'otm.urls'
+WSGI_APPLICATION = 'otm.wsgi.application'
+
+LANGUAGE_CODE = 'es-PE'
+USE_I18N = True
+USE_L10N = False
+TIME_ZONE = 'America/Lima'
+DATETIME_FORMAT = 'd/m/Y H:i'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, "templates"),],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
+
+COMPRESS_CSS_FILTERS = (
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+)
+
+COMPRESS_OUTPUT_DIR = ''
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+LOGIN_URL = '/auth/login'
+LOGIN_REDIRECT_URL = '/'
+#LOGOUT_REDIRECT_URL = '/'
--- otm/urls.py
+++ otm/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
 
 
 urlpatterns = [
@@ -22,3 +23,9 @@ urlpatterns = [
     url(r'^auth/', include('autenticacion.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
