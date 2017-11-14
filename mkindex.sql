1534ebb13bd35b0897bc9002fcc3cd945a6658b9
Fredy Quispe <fredysqm@gmail.com>
2017-11-14 11:54:11 -0500

    fts populate

--- mkindex.sql
+++ mkindex.sql
@@ -1,2 +1,8 @@
-ALTER TABLE `otm`.`mantenimiento_proveedor` ADD FULLTEXT INDEX `mantemiento_fulltext` (`_searchtext` ASC);
+ALTER TABLE `otm`.`mantenimiento_proveedor` ADD FULLTEXT INDEX `mantemiento_proveedor_fulltext` (`_searchtext` ASC);
 
+DELIMITER //
+CREATE PROCEDURE update_fts_index()
+BEGIN
+    UPDATE mantenimiento_proveedor SET _searchtext = CONCAT_WS(' ', id, razon_social, nro_documento);
+END //
+DELIMITER ;
