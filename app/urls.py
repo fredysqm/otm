0537f32fc47dbdb92327d6d2c406198a5a28ce71
Fredy Quispe <fredysqm@gmail.com>
2018-03-19 15:05:14 -0500

    avance mantenimientos

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from app.views import *
 
 urlpatterns = [
     url(r'^$', app_main.as_view(), name='app_main'),
+    url(r'^pais/$', app_mantenimiento_pais.as_view(), name='app_mantenimiento_pais'),
     #url(r'^proveedor/$', app_mantenimiento_proveedores.as_view(), name='app_mantenimiento_proveedores'),
 
     #url(r'^error404/$', error404.as_view()),
--- app/views.py
+++ app/views.py
@@ -1,15 +1,11 @@
 from django.views.generic import TemplateView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
-#from core.models import Proveedor
 
 
-#ROOT
+#Root
 class app_main(LoginRequiredMixin, TemplateView):
     template_name = 'app/main.html'
 
 #Mantenimiento
-# class app_mantenimiento_proveedores(LoginRequiredMixin, ListView):
-#     template_name = 'app/proveedores/main.html'
-#     model = Proveedor
-#     queryset = Proveedor.objects.select_related().order_by('-id')
-#     paginate_by = 50
+class app_mantenimiento_pais(LoginRequiredMixin, TemplateView):
+    template_name = 'app/mantenimiento/pais.html'
