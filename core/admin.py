b49ead93e57046f14b062440512f12b5f4df5316
Fredy Quispe <fredysqm@gmail.com>
2018-04-03 16:21:37 -0500

    proveedor

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from reversion.admin import VersionAdmin
-from core.models import Pais, Ciudad, Idioma, Moneda, Banco, Operador
+from core.models import Pais, Ciudad, Idioma, Moneda, Banco, Operador, TipoDocProveedor, Proveedor
 
 
 @admin.register(Pais)
@@ -45,4 +45,19 @@ class OperadorAdmin(VersionAdmin):
     list_display = ('id', 'nombre', '_creado','_modificado',)
     search_fields = ('id', 'nombre',)
     list_filter = ( '_estado_obj', '_creado', '_modificado' )
+    ordering = ['id']
+
+@admin.register(TipoDocProveedor)
+class TipoDocProveedorAdmin(VersionAdmin):
+    list_display = ('id', 'nombre', '_creado','_modificado',)
+    search_fields = ('id', 'nombre',)
+    list_filter = ( '_estado_obj', '_creado', '_modificado' )
+    ordering = ['id']
+
+@admin.register(Proveedor)
+class ProveedorAdmin(VersionAdmin):
+    list_display = ('tipo_documento', 'id', 'razon_social', '_creado','_modificado',)
+    exclude = ('_fts',)
+    search_fields = ('@_fts',)
+    list_filter = ( ('tipo_documento', admin.RelatedOnlyFieldListFilter), '_estado_obj', '_creado', '_modificado' )
     ordering = ['id']
