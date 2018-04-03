c45e06293f0dd529fb3b907ba619edb3aac98bfd
Fredy Quispe <fredysqm@gmail.com>
2018-04-03 10:15:59 -0500

    mejoras admin filtros

--- core/admin.py
+++ core/admin.py
@@ -7,37 +7,42 @@ from core.models import Pais, Ciudad, Idioma, Moneda, Banco, Operador
 class PaisAdmin(VersionAdmin):
     list_display = ('id', 'nombre', '_creado','_modificado',)
     search_fields = ('id', 'nombre',)
-    ordering = ['id']
+    list_filter = ( '_estado_obj', '_creado', '_modificado' )
+    ordering = ['nombre']
 
 @admin.register(Ciudad)
 class CiudadAdmin(VersionAdmin):
     list_display = ('id', 'pais', 'nombre', '_creado','_modificado',)
     list_select_related = ('pais', )
-    raw_id_fields = ('pais',)
+    #raw_id_fields = ('pais',)
     search_fields = ('id', 'pais__nombre', 'pais__id', 'nombre',)
-    list_filter = ( ('pais', admin.RelatedOnlyFieldListFilter), )
+    list_filter = ( ('pais', admin.RelatedOnlyFieldListFilter), '_estado_obj', '_creado', '_modificado' )
     ordering = ['id']
 
 @admin.register(Idioma)
 class IdiomaAdmin(VersionAdmin):
     list_display = ('id', 'nombre', '_creado','_modificado',)
     search_fields = ('id', 'nombre',)
+    list_filter = ( '_estado_obj', '_creado', '_modificado' )
     ordering = ['id']
 
 @admin.register(Moneda)
 class MonedaAdmin(VersionAdmin):
     list_display = ('id', 'nombre', 'simbolo', '_creado','_modificado',)
     search_fields = ('id', 'nombre',)
+    list_filter = ( '_estado_obj', '_creado', '_modificado' )
     ordering = ['id']
 
 @admin.register(Banco)
 class BancoaAdmin(VersionAdmin):
     list_display = ('id', 'nombre', '_creado','_modificado',)
     search_fields = ('id', 'nombre',)
+    list_filter = ( '_estado_obj', '_creado', '_modificado' )
     ordering = ['id']
 
 @admin.register(Operador)
 class OperadorAdmin(VersionAdmin):
     list_display = ('id', 'nombre', '_creado','_modificado',)
     search_fields = ('id', 'nombre',)
+    list_filter = ( '_estado_obj', '_creado', '_modificado' )
     ordering = ['id']
