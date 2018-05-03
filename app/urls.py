6dd302d03a1c676b6f3ebc8b3d55357398fc3d64
Fredy Quispe <fredysqm@gmail.com>
2018-05-03 16:46:40 -0500

    stable template bs4

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from app.views import *
 
 urlpatterns = [
     url(r'^$', app_main.as_view(), name='app_main'),
-    url(r'^proveedor/$', app_proveedor.as_view(), name='app_proveedor'),
+    url(r'^proveedores/$', app_proveedor.as_view(), name='app_proveedor'),
 
     #url(r'^error404/$', error404.as_view()),
     #url(r'^error500/$', error500.as_view()),
