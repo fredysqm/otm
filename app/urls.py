86910217d09305ca403ac48256878de16e576ec7
Fredy Quispe <fredysqm@gmail.com>
2018-05-03 16:16:00 -0500

    base funciona bs4

--- app/urls.py
+++ app/urls.py
@@ -4,13 +4,7 @@ from app.views import *
 
 urlpatterns = [
     url(r'^$', app_main.as_view(), name='app_main'),
-    url(r'^pais/$', app_mantenimiento_pais.as_view(), name='app_mantenimiento_pais'),
-    url(r'^ciudad/$', app_mantenimiento_ciudad.as_view(), name='app_mantenimiento_ciudad'),
-    url(r'^idioma/$', app_mantenimiento_idioma.as_view(), name='app_mantenimiento_idioma'),
-    url(r'^moneda/$', app_mantenimiento_moneda.as_view(), name='app_mantenimiento_moneda'),
-    url(r'^banco/$', app_mantenimiento_banco.as_view(), name='app_mantenimiento_banco'),
-    url(r'^operador/$', app_mantenimiento_operador.as_view(), name='app_mantenimiento_operador'),
-    url(r'^proveedor/$', app_mantenimiento_proveedor.as_view(), name='app_mantenimiento_proveedor'),
+    url(r'^proveedor/$', app_proveedor.as_view(), name='app_proveedor'),
 
     #url(r'^error404/$', error404.as_view()),
     #url(r'^error500/$', error500.as_view()),
--- app/views.py
+++ app/views.py
@@ -7,23 +7,5 @@ class app_main(LoginRequiredMixin, TemplateView):
     template_name = 'app/main.html'
 
 #Mantenimiento
-class app_mantenimiento_pais(LoginRequiredMixin, TemplateView):
-    template_name = 'app/mantenimiento/pais.html'
-
-class app_mantenimiento_ciudad(LoginRequiredMixin, TemplateView):
-    template_name = 'app/mantenimiento/ciudad.html'
-
-class app_mantenimiento_idioma(LoginRequiredMixin, TemplateView):
-    template_name = 'app/mantenimiento/idioma.html'
-
-class app_mantenimiento_moneda(LoginRequiredMixin, TemplateView):
-    template_name = 'app/mantenimiento/moneda.html'
-
-class app_mantenimiento_banco(LoginRequiredMixin, TemplateView):
-    template_name = 'app/mantenimiento/banco.html'
-
-class app_mantenimiento_operador(LoginRequiredMixin, TemplateView):
-    template_name = 'app/mantenimiento/operador.html'
-
-class app_mantenimiento_proveedor(LoginRequiredMixin, TemplateView):
-    template_name = 'app/mantenimiento/proveedor.html'
+class app_proveedor(LoginRequiredMixin, TemplateView):
+    template_name = 'app/proveedor/main.html'
