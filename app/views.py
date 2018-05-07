ca8218d6e16bcdf3fb7cf7bd9db70e47366e0456
Fredy Quispe <fredysqm@gmail.com>
2018-05-07 17:08:37 -0500

    previo, eliminacion underscores

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from core.models import MarcaComercial
 
 
 #Root
@@ -7,5 +8,6 @@ class app_main(LoginRequiredMixin, TemplateView):
     template_name = 'app/main.html'
 
 #Mantenimiento
-class app_proveedor(LoginRequiredMixin, TemplateView):
+class app_proveedor(LoginRequiredMixin, ListView):
+    model = MarcaComercial
     template_name = 'app/proveedor/main.html'
