e47c7e4317a80ff0fda0f29a4cb29bdb343f0a84
Fredy Quispe <fredysqm@gmail.com>
2018-05-15 11:56:11 -0500

    limpieza

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,6 @@ from app.views import *
 urlpatterns = [
     path('', app_main.as_view(), name='app_main'),
     path('proveedores/', app_proveedor.as_view(), name='app_proveedor'),
-    path('proveedores/crear/', app_proveedor_create.as_view(), name='app_proveedor_create'),
     #url(r'^error404/$', error404.as_view()),
     #url(r'^error500/$', error500.as_view()),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from core.models import MarcaComercial
@@ -12,10 +12,3 @@ class app_main(LoginRequiredMixin, TemplateView):
 #Mantenimiento
 class app_proveedor(LoginRequiredMixin, TemplateView):
     template_name = 'app/proveedor/main.html'
-
-class app_proveedor_create(LoginRequiredMixin, CreateView):
-    #model = MarcaComercial
-    form_class = ProveedorCrearForm
-    #fields = ('nombre', 'proveedor', 'categoria_servicio', 'localidad', 'modalidad_pago', 'direccion', 'telefono_fijo', 'telefono_movil', 'email', 'sitio_web', 'observaciones')
-    template_name = 'app/proveedor/create.html'
-    success_url = reverse_lazy('app_proveedor')
