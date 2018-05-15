710e99d8ed9fed7e7615cd56cd80175eecd2ecb2
Fredy Quispe <fredysqm@gmail.com>
2018-05-15 15:39:18 -0500

    avance

--- app/views.py
+++ app/views.py
@@ -2,7 +2,6 @@ from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from core.models import MarcaComercial
-from app.forms import ProveedorCrearForm
 
 
 #Root
